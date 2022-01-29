28c3bbe8388ffebee92199f61d5df18478761ddc
Pratiksinh Makwana <pratikmak2542@gmail.com>
2022-01-29 22:01:29 +0530

    final version

---
+++ public/javascripts/app.js
@@ -0,0 +1,1 @@
+console.log('Goes to client side');
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let userController = require('../controllers/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.user);
 
 /* GET users listing. */
-router.get('/pratik', function(req, res, next) {
-  res.send('Welcome Boss !');
-});
+router.get('/pratik', userController.pratik);
 
 module.exports = router;
