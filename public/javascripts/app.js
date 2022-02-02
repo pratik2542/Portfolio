acd02bcbb3fd472d34cc1a526c9e2114c47ca5b8
Pratiksinh Makwana <pratikmak2542@gmail.com>
2022-02-03 00:03:28 +0530

    Final Done

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,11 @@
-console.log('Testing');
+/*
+File Name - app.js
+Student Name - Pratiksinh Makwana
+Student ID - 301219863
+Date - 29-01-2022
+*/
+
+
+//testing script
+
+console.log('Testing My WebSite !!');
